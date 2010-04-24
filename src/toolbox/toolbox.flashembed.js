19ea2021312f1e846c3e19d9267de4f9ccf32c04
Tero Piirainen <tipiirai@mitte.(none)>
2010-04-24 11:00:27 +0000

    flashembed rewrite. 24% smaller!

--- src/toolbox/toolbox.flashembed.js
+++ src/toolbox/toolbox.flashembed.js
@@ -9,435 +9,286 @@
  * Since : March 2008
  * Date  : @DATE 
  */ 
-(function() {  
+(function() {
 		
-//{{{ utility functions 
-		
-var jQ = typeof jQuery == 'function';
-
-var options = {
+	var IE = document.all,
+		 URL = 'http://www.adobe.com/go/getflashplayer',
+		 JQUERY = typeof jQuery == 'function', 
+		 RE = /(\d+)[^\d]+(\d+)[^\d]*(\d*)/,
+		 GLOBAL_OPTS = { 
+			// very common opts
+			width: '100%',
+			height: '100%',		
+			id: "_" + ("" + Math.random()).slice(9),
+			
+			// flashembed defaults
+			allowfullscreen: true,
+			allowscriptaccess: 'always',
+			quality: 'high',	
+			
+			// flashembed specific options
+			version: [3, 0],
+			onFail: null,
+			expressInstall: null, 
+			w3c: false,
+			cachebusting: false  		 		 
+	};
 	
-	// very common opts
-	width: '100%',
-	height: '100%',		
-	
-	// flashembed defaults
-	allowfullscreen: true,
-	allowscriptaccess: 'always',
-	quality: 'high',	
-	
-	// flashembed specific options
-	version: null,
-	onFail: null,
-	expressInstall: null, 
-	w3c: false,
-	cachebusting: false 
-};
-
-if (jQ) {
-		
-	// tools version number
-	jQuery.tools = jQuery.tools || {version: '@VERSION'};
-	
-	jQuery.tools.flashembed = {  
-		conf: options
-	};		
-}
-
-
-// from "Pro JavaScript techniques" by John Resig
-function isDomReady() {
-	
-	if (domReady.done)  { return false; }
-	
-	var d = document;
-	if (d && d.getElementsByTagName && d.getElementById && d.body) {
-		clearInterval(domReady.timer);
-		domReady.timer = null;
-		
-		for (var i = 0; i < domReady.ready.length; i++) {
-			domReady.ready[i].call();	
-		}
-		
-		domReady.ready = null;
-		domReady.done = true;
-	} 
-}
-
-// if jQuery is present, use it's more effective domReady method
-var domReady = jQ ? jQuery : function(f) {
-	
-	if (domReady.done) {
-		return f();	
+	// version 9 bugfix: (http://blog.deconcept.com/2006/07/28/swfobject-143-released/)
+	if (window.attachEvent) {
+		window.attachEvent("onbeforeunload", function() {
+			__flash_unloadHandler = function() {};
+			__flash_savedUnloadHandler = function() {};
+		});
 	}
 	
-	if (domReady.timer) {
-		domReady.ready.push(f);	
-		
-	} else {
-		domReady.ready = [f];
-		domReady.timer = setInterval(isDomReady, 13);
-	} 
-};	
-
-
-// override extend opts function 
-function extend(to, from) {
-	if (from) {
-		for (key in from) {
-			if (from.hasOwnProperty(key)) {
-				to[key] = from[key];
-			}
-		}
-	}
-	
-	return to;
-}	
-
-
-// JSON.asString() function
-function asString(obj) {
-	
-	switch (typeOf(obj)){  
-		
-		case 'string':
-			obj = obj.replace(new RegExp('(["\\\\])', 'g'), '\\$1');
-			
-			// flash does not handle %- characters well. transforms "50%" to "50pct" (a dirty hack, I admit)
-			obj = obj.replace(/^\s?(\d+\.?\d+)%/, "$1pct");
-			return '"' +obj+ '"';
-			
-		case 'array':
-			return '['+ map(obj, function(el) {
-				return asString(el);
-			}).join(',') +']'; 
-			
-		case 'function':
-			return '"function()"';
-			
-		case 'object':
-			var str = [];
-			for (var prop in obj) {
-				if (obj.hasOwnProperty(prop)) {
-					str.push('"'+prop+'":'+ asString(obj[prop]));
+	// simple extend
+	function extend(to, from) {
+		if (from) {
+			for (key in from) {
+				if (from.hasOwnProperty(key)) {
+					to[key] = from[key];
 				}
 			}
-			return '{'+str.join(',')+'}';
-	}
-	
-	// replace ' --> "  and remove spaces
-	return String(obj).replace(/\s/g, " ").replace(/\'/g, "\"");
-}
-
-
-// private functions
-function typeOf(obj) {
-	if (obj === null || obj === undefined) { return false; }
-	var type = typeof obj;
-	return (type == 'object' && obj.push) ? 'array' : type;
-}
-
-
-// version 9 bugfix: (http://blog.deconcept.com/2006/07/28/swfobject-143-released/)
-if (window.attachEvent) {
-	window.attachEvent("onbeforeunload", function() {
-		__flash_unloadHandler = function() {};
-		__flash_savedUnloadHandler = function() {};
-	});
-}
-
-function map(arr, func) {
-	var newArr = []; 
-	for (var i in arr) {
-		if (arr.hasOwnProperty(i)) {
-			newArr[i] = func(arr[i]);
-		}
-	}
-	return newArr;
-}
-	
-function getHTML(p, c) {
-		
-	var e = extend({}, p);	 
-	var ie = document.all;	
-	var html = '<object width="' +e.width+ '" height="' +e.height+ '"';
-	
-	// force id for IE or Flash API cannot be returned
-	if (ie && !e.id) {
-		e.id = "_" + ("" + Math.random()).slice(9);
-	}
-	
-	if (e.id) {	
-		html += ' id="' + e.id + '"';	
-	}
-	
-	// prevent possible caching problems
-	if (e.cachebusting) {
-		e.src += ((e.src.indexOf("?") != -1 ? "&" : "?") + Math.random());		
-	}			
-	
-	if (e.w3c || !ie) {
-		html += ' data="' +e.src+ '" type="application/x-shockwave-flash"';		
-	} else {
-		html += ' classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"';	
-	}
-	
-	html += '>'; 
-	
-	if (e.w3c || ie) {
-		html += '<param name="movie" value="' +e.src+ '" />'; 	
-	}
-
-	// parameters
-	e.width = e.height = e.id = e.w3c = e.src = null;
-	
-	for (var k in e) {
-		if (e[k] !== null) {
-			html += '<param name="'+ k +'" value="'+ e[k] +'" />';
-		}
+		} 
+		return to;
 	}	
 
-	// flashvars
-	var vars = "";
-	
-	if (c) {
-		for (var key in c) {
-			var val = c[key];
-			if (val !== null) {
-				vars += key +'='+ (typeof val == 'object' || typeof val == 'function' ? asString(val) : val) + '&';
+	// used by asString method	
+	function map(arr, func) {
+		var newArr = []; 
+		for (var i in arr) {
+			if (arr.hasOwnProperty(i)) {
+				newArr[i] = func(arr[i]);
 			}
 		}
-		vars = vars.slice(0, -1);
-		html += '<param name="flashvars" value=\'' + vars + '\' />';
+		return newArr;
 	}
-	
-	html += "</object>";	
-	
-	return html;
 
-}
-
-//}}}
-
-
-function Flash(root, opts, flashvars) {
+	window.flashembed = function(root, opts, conf) {
 	
-	var version = flashembed.getVersion(); 
-	
-	// API methods for callback
-	extend(this, {
-			
-		getContainer: function() {
-			return root;	
-		},
-		
-		getConf: function() {
-			return opts;	
-		},
-	
-		getVersion: function() {
-			return version;	
-		},	
-		
-		getFlashvars: function() {
-			return flashvars;	
-		}, 
-		
-		getApi: function() {
-			return root.firstChild;	
-		}, 
-		
-		getHTML: function() {
-			return getHTML(opts, flashvars);	
+		// root must be found / loaded	
+		if (typeof root == 'string') {
+			root = document.getElementById(root.replace("#", ""));
 		}
 		
-	});
-
-	// variables	
-	var required = opts.version; 
-	var express = opts.expressInstall;
-	
-	
-	// everything ok -> generate OBJECT tag 
-	var ok = !required || flashembed.isSupported(required);
-	
-	if (ok) {
-		opts.onFail = opts.version = opts.expressInstall = null;
-		root.innerHTML = getHTML(opts, flashvars);
+		// not found
+		if (!root) { return; }
 		
-	// fail #1. express install
-	} else if (required && express && flashembed.isSupported([6,65])) {
-		
-		extend(opts, {src: express});
-		
-		flashvars = {
-			MMredirectURL: location.href,
-			MMplayerType: 'PlugIn',
-			MMdoctitle: document.title
-		};
-		
-		root.innerHTML = getHTML(opts, flashvars);	
-		
-	// fail #2. 
-	} else { 
-	
-		// fail #2.1 custom content inside container
-		if (root.innerHTML.replace(/\s/g, '') !== '') {
-			// minor bug fixed here 08.04.2008 (thanks JRodman)			
-		
-		// fail #2.2 default content
-		} else {			
-			root.innerHTML = 
-				"<h2>Flash version " + required + " or greater is required</h2>" + 
-				"<h3>" + 
-					(version[0] > 0 ? "Your version is " + version : "You have no flash plugin installed") +
-				"</h3>" + 
-				
-				(root.tagName == 'A' ? "<p>Click here to download latest version</p>" : 
-					"<p>Download latest version from <a href='http://www.adobe.com/go/getflashplayer'>here</a></p>");
-				
-			if (root.tagName == 'A') {	
-				root.onclick = function() {
-					location.href= 'http://www.adobe.com/go/getflashplayer';
-				};
-			}				
+		if (typeof opts == 'string') {
+			opts = {src: opts};	
 		}
-	}
-	
-	// onFail
-	if (!ok && opts.onFail) {
-		var ret = opts.onFail.call(this);
-		if (typeof ret == 'string') { root.innerHTML = ret; }	
-	}
-	
-	// http://flowplayer.org/forum/8/18186#post-18593
-	if (document.all) {
-		window[opts.id] = document.getElementById(opts.id);
-	} 
-	
-}
 
-window.flashembed = function(root, conf, flashvars) {   
+		return new Flash(root, extend(extend({}, GLOBAL_OPTS), opts), conf); 
+	};	
 	
-//{{{ construction
-	
-	// root must be found / loaded	
-	if (typeof root == 'string') {
-		var el = document.getElementById(root);
-		if (el) {
-			root = el;	
-		} else {
-			domReady(function() {
-				flashembed(root, conf, flashvars);
-			});
-			return; 		
-		} 
-	}
-	
-	// not found
-	if (!root) { return; }
-	
-	if (typeof conf == 'string') {
-		conf = {src: conf};	
-	}
-	
-	var opts = extend({}, options);
-	extend(opts, conf);		
-	
-	return new Flash(root, opts, flashvars);
-	
-//}}}
-	
-	
-};
-
-
-//{{{ static methods
-
-extend(window.flashembed, {
-
-	// returns arr[major, fix]
-	getVersion: function() {
-	
-		var version = [0, 0];
+	// flashembed "static" API
+	var f = extend(window.flashembed, {
 		
-		if (navigator.plugins && typeof navigator.plugins["Shockwave Flash"] == "object") {
-			var _d = navigator.plugins["Shockwave Flash"].description;
-			if (typeof _d != "undefined") {
-				_d = _d.replace(/^.*\s+(\S+\s+\S+$)/, "$1");
-				var _m = parseInt(_d.replace(/^(.*)\..*$/, "$1"), 10);
-				var _r = /r/.test(_d) ? parseInt(_d.replace(/^.*r(.*)$/, "$1"), 10) : 0;
-				version = [_m, _r];
-			}
+		conf: GLOBAL_OPTS,
+	
+		getVersion: function()  {
+			var ver;
 			
-		} else if (window.ActiveXObject) {
-
-			try { // avoid fp 6 crashes
-				var _a = new ActiveXObject("ShockwaveFlash.ShockwaveFlash.7");
-				
+			try {
+				ver = navigator.plugins["Shockwave Flash"].description.slice(16); 
 			} catch(e) {
 				
-				try { 
-					_a = new ActiveXObject("ShockwaveFlash.ShockwaveFlash.6");
-					version = [6, 0];
-					_a.AllowScriptAccess = "always"; // throws if fp < 6.47 
-					
-				} catch(ee) {
-					if (version[0] == 6) { return version; }
-				}
-				try {
-					_a = new ActiveXObject("ShockwaveFlash.ShockwaveFlash");
-				} catch(eee) {
-				
-				}
-				
+				try  {
+					var fo = new ActiveXObject("ShockwaveFlash.ShockwaveFlash.7");
+					ver = fo && fo.GetVariable("$version");	 
+				} catch(err) {
+						
+				} 
 			}
 			
-			if (typeof _a == "object") {
-				_d = _a.GetVariable("$version"); // bugs in fp 6.21 / 6.23
-				if (typeof _d != "undefined") {
-					_d = _d.replace(/^\S+\s+(.*)$/, "$1").split(",");
-					version = [parseInt(_d[0], 10), parseInt(_d[2], 10)];
-				}
+			ver = RE.exec(ver);
+			return [ver[1], ver[3]];
+		},
+		
+		asString: function(obj) { 
+
+			if (obj === null || obj === undefined) { return ""; }
+			var type = typeof obj;
+			if (type == 'object' && obj.push) { type = 'array'; }
+			
+			switch (type){  
+				
+				case 'string':
+					obj = obj.replace(new RegExp('(["\\\\])', 'g'), '\\$1');
+					
+					// flash does not handle %- characters well. transforms "50%" to "50pct" (a dirty hack, I admit)
+					obj = obj.replace(/^\s?(\d+\.?\d+)%/, "$1pct");
+					return '"' +obj+ '"';
+					
+				case 'array':
+					return '['+ map(obj, function(el) {
+						return f.asString(el);
+					}).join(',') +']'; 
+					
+				case 'function':
+					return '"function()"';
+					
+				case 'object':
+					var str = [];
+					for (var prop in obj) {
+						if (obj.hasOwnProperty(prop)) {
+							str.push('"'+prop+'":'+ f.asString(obj[prop]));
+						}
+					}
+					return '{'+str.join(',')+'}';
 			}
+			
+			// replace ' --> "  and remove spaces
+			return String(obj).replace(/\s/g, " ").replace(/\'/g, "\"");
+		},
+		
+		getHTML: function(opts, conf) {
+
+			opts = extend({}, opts);
+			
+			/******* OBJECT tag and it's attributes *******/
+			var html = '<object width="' + opts.width + '" height="' + opts.height + '" id="' + opts.id + '"';
+			
+			if (opts.cachebusting) {
+				opts.src += ((opts.src.indexOf("?") != -1 ? "&" : "?") + Math.random());		
+			}			
+			
+			if (opts.w3c || !IE) {
+				html += ' data="' +opts.src+ '" type="application/x-shockwave-flash"';		
+			} else {
+				html += ' classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"';	
+			}
+			
+			html += '>'; 
+			
+			/******* nested PARAM tags *******/
+			if (opts.w3c || IE) {
+				html += '<param name="movie" value="' +opts.src+ '" />'; 	
+			} 
+			
+			// not allowed params
+			opts.width = opts.height = opts.id = opts.w3c = opts.src = null;
+			opts.onFail = opts.version = opts.expressInstall = null;
+			
+			for (var key in opts) {
+				if (opts[key]) {
+					html += '<param name="'+ key +'" value="'+ opts[key] +'" />';
+				}
+			}	
+		
+			/******* FLASHVARS *******/
+			var vars = "";
+			
+			if (conf) {
+				for (var k in conf) { 
+					if (conf[k]) {
+						var val = conf[k]; 
+						vars += k +'='+ (/function|object/.test(typeof val) ? f.asString(val) : val) + '&';
+					}
+				}
+				vars = vars.slice(0, -1);
+				html += '<param name="flashvars" value=\'' + vars + '\' />';
+			}
+			
+			html += "</object>";	
+			
+			return html;				
+		},
+		
+		isSupported: function(ver) {
+			return VERSION[0] > ver[0] || VERSION[0] == ver[0] && VERSION[1] >= ver[1];			
+		}		
+		
+	});
+	
+	var VERSION = f.getVersion(); 
+	
+	function Flash(root, opts, conf) {  
+	                                                
+		// version is ok
+		if (f.isSupported(opts.version)) {
+			root.innerHTML = f.getHTML(opts, conf);
+			
+		// express install
+		} else if (opts.expressInstall && f.isSupported([6, 65])) {
+			root.innerHTML = f.getHTML(extend(opts, {src: opts.expressInstall}), {
+				MMredirectURL: location.href,
+				MMplayerType: 'PlugIn',
+				MMdoctitle: document.title
+			});	
+			
+		} else {
+			
+			// fail #2.1 custom content inside container
+			if (!root.innerHTML.replace(/\s/g, '')) {
+				root.innerHTML = 
+					"<h2>Flash version " + opts.version + " or greater is required</h2>" + 
+					"<h3>" + 
+						(VERSION[0] > 0 ? "Your version is " + VERSION : "You have no flash plugin installed") +
+					"</h3>" + 
+					
+					(root.tagName == 'A' ? "<p>Click here to download latest version</p>" : 
+						"<p>Download latest version from <a href='" + URL + "'>here</a></p>");
+					
+				if (root.tagName == 'A') {	
+					root.onclick = function() {
+						location.href = URL;
+					};
+				}				
+			}
+			
+			// onFail
+			if (opts.onFail) {
+				var ret = opts.onFail.call(this);
+				if (typeof ret == 'string') { root.innerHTML = ret; }	
+			}			
+		}
+		
+		// http://flowplayer.org/forum/8/18186#post-18593
+		if (IE) {
+			window[opts.id] = document.getElementById(opts.id);
 		} 
 		
-		return version;
-	},
-	
-	isSupported: function(version) {
-		var now = flashembed.getVersion();
-		var ret = (now[0] > version[0]) || (now[0] == version[0] && now[1] >= version[1]);			
-		return ret;
-	},
-	
-	domReady: domReady,
-	
-	// returns a String representation from JSON object 
-	asString: asString,
-	
-	
-	getHTML: getHTML
-	
-});
+		// API methods for callback
+		extend(this, {
+				
+			getRoot: function() {
+				return root;	
+			},
+			
+			getOptions: function() {
+				return opts;	
+			},
 
-//}}}
-
-
-// setup jquery support
-if (jQ) {
+			
+			getConf: function() {
+				return conf;	
+			}, 
+			
+			getApi: function() {
+				return root.firstChild;	
+			}
+			
+		}); 
+	}
 	
-	jQuery.fn.flashembed = function(conf, flashvars) {
+	// setup jquery support
+	if (JQUERY) {
 		
-		var el = null;
+		// tools version number
+		jQuery.tools = jQuery.tools || {version: '@VERSION'};
 		
-		this.each(function() { 
-			el = flashembed(this, conf, flashvars);
-		});
+		jQuery.tools.flashembed = {  
+			conf: GLOBAL_OPTS
+		};	
 		
-		return conf.api === false ? this : el;		
-	};
-
-}
-
+		jQuery.fn.flashembed = function(opts, conf) {		
+			return this.each(function() { 
+				$(this).data("flashembed", flashembed(this, opts, conf));
+			});
+		}; 
+	} 
+	
 })();
+
