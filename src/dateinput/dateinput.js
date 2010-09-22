3e1e522bc3ae11e99f4f609c7714eeac88fa74d1
Tero Piirainen <tipiirai@gmail.com>
2010-09-22 06:36:38 +0000

    passing lint

--- src/dateinput/dateinput.js
+++ src/dateinput/dateinput.js
@@ -459,9 +459,8 @@
 
 			setValue: function(year, month, day)  {
 				
-				var date = integer(month) >= -1 ?
-					new Date(integer(year), integer(month), integer(day || 1)) : year || value
-				;				
+				var date = integer(month) >= -1 ? new Date(integer(year), integer(month), integer(day || 1)) : 
+					year || value;				
 				
 				if (date < min) { date = min; }
 				else if (date > max) { date = max; }
