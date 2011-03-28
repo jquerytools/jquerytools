695f13a1288183a85c9450432488b55e8716e388
ewheeler <evanmwheeler@gmail.com>
2011-03-28 13:38:49 -0400

    check each tooltip for existence, fixes issue 349
    rather than checking if any tooltips already exist.
    this way tooltips on some elements that are reloaded via
    ajax calls will continue working. the previous code would
    assume all tooltips still exist if *any* of the tooltips still
    exist -- even if some of them no longer exist. now, part of a
    page may be changed and can have new tooltips created instead
    of only the ones for elements that did not change.
    
    resolves: https://github.com/jquerytools/jquerytools/issues/349
    
    Signed-off-by: Adam Mckaig <adam.mckaig@gmail.com>

--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -332,10 +332,6 @@
 	
 	// jQuery plugin implementation
 	$.fn.tooltip = function(conf) {
-		
-		// return existing instance
-		var api = this.data("tooltip");
-		if (api) { return api; }
 
 		conf = $.extend(true, {}, $.tools.tooltip.conf, conf);
 		
@@ -345,9 +341,12 @@
 		}
 		
 		// install tooltip for each entry in jQuery object
+		// that is not an existing instance
 		this.each(function() {
-			api = new Tooltip($(this), conf); 
-			$(this).data("tooltip", api); 
+			if ( $(this).data("tooltip")===null){
+			    api = new Tooltip($(this), conf);
+			    $(this).data("tooltip", api);
+			};
 		});
 		
 		return conf.api ? api: this;		 
