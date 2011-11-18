7c3f8f3f2b3ead4bc9c49462b8e0b982fd10ef4c
cpietsch <cpietsch@gmail.com>
2011-11-18 12:45:25 +0100

    added makeCoolPagination which turns 1 2 3 4 5 6 7 into 1 ... 3 4 5 ... 7

--- src/scrollable/scrollable.navigator.js
+++ src/scrollable/scrollable.navigator.js
@@ -21,6 +21,7 @@
 			activeClass: 'active',
 			indexed: false,
 			idPrefix: null,
+	
 			
 			// 1.2
 			history: false
@@ -47,9 +48,7 @@
 				 navi = conf.navi.jquery ? conf.navi : find(api.getRoot(), conf.navi), 
 				 buttons = api.getNaviButtons(),
 				 cls = conf.activeClass,
-				 hashed = conf.history && !!history.pushState,
-				 size = api.getConf().size;
-				 
+				 history = conf.history && $.fn.history;
 
 			// @deprecated stuff
 			if (api) { ret = api; }
@@ -59,39 +58,70 @@
 			}; 
 			
 			
-			if (hashed) {
-				history.pushState({i: 0});
-				
-				$(window).bind("popstate", function(evt) {
-					var s = evt.originalEvent.state;
-					if (s) { api.seekTo(s.i); }
-				});					
-			}
-			
 			function doClick(el, i, e) {
-				api.seekTo(i);
-				e.preventDefault(); 
-				if (hashed) { history.pushState({i: i}); }
+				api.seekTo(i);				
+				if (history) {
+					if (location.hash) {
+						location.hash = el.attr("href").replace("#", "");	
+					}
+				} else  {
+					return e.preventDefault();			
+				}
 			}
 			
 			function els() {
-				return navi.find(conf.naviItem || '> *');	
+				return navi.find('.navItem');	
 			}
+
 			
 			function addItem(i) {  
-				
+							
 				var item = $("<" + (conf.naviItem || 'a') + "/>").click(function(e)  {
-					doClick($(this), i, e);					
-				});
+					doClick($(this), i, e);
+					
+				}).attr("href", "#" + i).addClass('navItem');
 				
 				// index number / id attribute
 				if (i === 0) {  item.addClass(cls); }
 				if (conf.indexed)  { item.text(i + 1); }
-				if (conf.idPrefix) { item.attr("id", conf.idPrefix + i); }
+				if (conf.idPrefix) { item.attr("id", conf.idPrefix + i); } 
+				
+				if(i==1 || i==api.getSize()-1){
+					$("<" + (conf.naviItem) + "/>").addClass('points').appendTo(navi).text("…").hide();
+				}
 				
 				return item.appendTo(navi);
 			}
 			
+			function makeCoolPagination(){
+				
+				// hide points
+				navi.find('.points').hide();
+				
+				els().each(function(i,elm) { 
+					if(i==0 || i==api.getSize()-1){
+						// für den ersten und letzten
+						$(elm).show();
+					} else {
+						if(Math.abs(i-api.getIndex())<2){
+							$(elm).show();
+						} else {
+							$(elm).hide();
+							
+						}
+					}
+					if(api.getIndex()>2){
+						navi.find('.points:first').show();
+					}
+					if(api.getSize()-api.getIndex()>3){
+						navi.find('.points:last').show();
+					}
+					
+				});
+				
+				
+			}
+
 			
 			// generate navigator
 			if (els().length) {
@@ -101,29 +131,43 @@
 					});
 				});
 				
-			} else {				
+			} else {
 				$.each(api.getItems(), function(i) {
-					if (i % size == 0) addItem(i); 
+					addItem(i); 
 				});
 			}   
 			
+			// Make cool pagination
+			makeCoolPagination();
+		
 			// activate correct entry
 			api.onBeforeSeek(function(e, index) {
 				setTimeout(function() {
-					if (!e.isDefaultPrevented()) {	
-						var i = index / size,
-							 el = els().eq(i);
-							 
-						if (el.length) { els().removeClass(cls).eq(i).addClass(cls); }
+					if (!e.isDefaultPrevented()) {
+						makeCoolPagination();
+						var el = els().eq(index);
+						if (!e.isDefaultPrevented() && el.length) {			
+							els().removeClass(cls).eq(index).addClass(cls);
+						}
 					}
 				}, 1);
 			}); 
 			
+			function doHistory(evt, hash) {
+				var el = els().eq(hash.replace("#", ""));
+				if (!el.length) {
+					el = els().filter("[href=" + hash + "]");	
+				}
+				el.click();		
+			}
+			
 			// new item being added
 			api.onAddItem(function(e, item) {
-				var i = api.getItems().index(item);
-				if (i % size == 0) addItem(i);
+				item = addItem(api.getItems().index(item)); 
+				if (history)  { item.history(doHistory); }
 			});
+			
+			if (history) { els().history(doHistory); }
 			
 		});		
 		
@@ -131,4 +175,4 @@
 		
 	};
 	
-})(jQuery);			
+})(jQuery);	
