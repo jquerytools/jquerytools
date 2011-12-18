66cdb8a3a02a30848d3e5909e9d4dfda3fe663f9
Duc Tri Le <github@email.tiger-inc.com>
2011-12-18 15:56:43 -0500

    #480: Fixing readonly bug

--- src/dateinput/dateinput.js
+++ src/dateinput/dateinput.js
@@ -289,7 +289,12 @@
 		
 //{{{ pick
 			 			 
-		function select(date, conf, e) {  
+		function select(date, conf, e) {
+			// If it is readonly, then we'll just close the calendar
+			if (input.attr('readonly')) {
+				self.hide(e);
+				return;
+			}
 			
 			// current value
 			value 	 = date;
@@ -420,7 +425,7 @@
 			*/					
 			show: function(e) {
 				
-				if (input.attr("readonly") || input.attr("disabled") || opened) { return; }
+				if (input.attr("disabled") || opened) { return; }
 				
 				// onBeforeShow
 				e = e || $.Event();
