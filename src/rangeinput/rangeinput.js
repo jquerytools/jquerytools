d49c630e1786863274f54b22e87c04d39999c59b
J Bruni <joaohbruni@yahoo.com.br>
2013-08-20 00:33:44 -0300

    Finish blank space madness
    Removed trailing blank spaces.
    
    Stardardized indentation style (only tabs, instead of mixing tabs with spaces).
    
    This commit does not change any code - it only deals with blank spaces!
    
    The only exception is a typo fix in a comment.

--- src/rangeinput/rangeinput.js
+++ src/rangeinput/rangeinput.js
@@ -1,43 +1,42 @@
 /**
- * @license 
+ * @license
  * jQuery Tools @VERSION Rangeinput - HTML5 <input type="range" /> for humans
  * 
  * NO COPYRIGHTS OR LICENSES. DO WHAT YOU LIKE.
  * 
  * http://flowplayer.org/tools/rangeinput/
- *
+ * 
  * Since: Mar 2010
  * Date: @DATE 
  */
- 
- var oldFnData = jQuery.fn.data;
 
-jQuery.fn.data = function( name ) {
-  var ret, evt,
-    elem = this[0];
+var oldFnData = jQuery.fn.data;
 
-  // Handles 1.7 which has this behavior and 1.8 which doesn't
-  if ( elem && name === "events" && arguments.length === 1 ) {
-    ret = jQuery.data( elem, name );
-    evt = jQuery._data( elem, name );
-  }
-  return oldFnData.apply( this, arguments );
+jQuery.fn.data = function(name) {
+	var ret, evt,
+	    elem = this[0];
+	// Handles 1.7 which has this behavior and 1.8 which doesn't
+	if (elem && name === 'events' && arguments.length === 1) {
+		ret = jQuery.data(elem, name);
+		evt = jQuery._data(elem, name);
+	}
+	return oldFnData.apply(this, arguments);
 };
- 
+
 (function($) {
-	 
+	
 	$.tools = $.tools || {version: '@VERSION'};
-	 
+	
 	var tool;
 	
 	tool = $.tools.rangeinput = {
-		                            
+		
 		conf: {
 			min: 0,
-			max: 100,		// as defined in the standard
-			step: 'any', 	// granularity of the value. a non-zero float or int (or "any")
+			max: 100,    // as defined in the standard
+			step: 'any', // granularity of the value. a non-zero float or int (or "any")
 			steps: 0,
-			value: 0,			
+			value: 0,
 			precision: undefined,
 			vertical: 0,
 			keyboard: true,
@@ -46,17 +45,17 @@ jQuery.fn.data = function( name ) {
 			
 			// set to null if not needed
 			css: {
-				input:		'range',
-				slider: 		'slider',
-				progress: 	'progress',
-				handle: 		'handle'					
+				input:    'range',
+				slider:   'slider',
+				progress: 'progress',
+				handle:   'handle'
 			}
-
-		} 
+			
+		}
 	};
-	
+
 //{{{ fn.drag
-		
+
 	/* 
 		FULL featured drag and drop. 0.7 kb minified, 0.3 gzipped. done.
 		Who told d'n'd is rocket science? Usage:
@@ -64,13 +63,13 @@ jQuery.fn.data = function( name ) {
 		$(".myelement").drag({y: false}).on("drag", function(event, x, y) {
 			// do your custom thing
 		});
-		 
+		
 		Configuration: 
-			x: true, 		// enable horizontal drag
-			y: true, 		// enable vertical drag 
-			drag: true 		// true = perform drag, false = only fire events 
+			x: true,     // enable horizontal drag
+			y: true,     // enable vertical drag
+			drag: true   // true = perform drag, false = only fire events
 			
-		Events: dragStart, drag, dragEnd. 
+		Events: dragStart, drag, dragEnd.
 	*/
 	var doc, draggable;
 	
@@ -80,26 +79,26 @@ jQuery.fn.data = function( name ) {
 		document.ondragstart = function () { return false; };
 		
 		conf = $.extend({x: true, y: true, drag: true}, conf);
-	
+		
 		doc = doc || $(document).on("mousedown mouseup", function(e) {
-				
-			var el = $(e.target);  
+			
+			var el = $(e.target);
 			
 			// start 
 			if (e.type == "mousedown" && el.data("drag")) {
 				
 				var offset = el.position(),
-					 x0 = e.pageX - offset.left, 
+					 x0 = e.pageX - offset.left,
 					 y0 = e.pageY - offset.top,
 					 start = true;    
 				
-				doc.on("mousemove.drag", function(e) {  
-					var x = e.pageX -x0, 
+				doc.on("mousemove.drag", function(e) {
+					var x = e.pageX -x0,
 						 y = e.pageY -y0,
 						 props = {};
 					
 					if (conf.x) { props.left = x; }
-					if (conf.y) { props.top = y; } 
+					if (conf.y) { props.top = y; }
 					
 					if (start) {
 						el.trigger("dragStart");
@@ -115,22 +114,21 @@ jQuery.fn.data = function( name ) {
 			} else {
 				
 				try {
-					if (draggable) {  
-						draggable.trigger("dragEnd");  
+					if (draggable) {
+						draggable.trigger("dragEnd");
 					}
-				} finally { 
+				} finally {
 					doc.off("mousemove.drag");
-					draggable = null; 
+					draggable = null;
 				}
-			} 
-							
+			}
+			
 		});
 		
-		return this.data("drag", true); 
-	};	
+		return this.data("drag", true);
+	};
 
 //}}}
-	
 
 	
 	function round(value, precision) {
@@ -143,7 +141,7 @@ jQuery.fn.data = function( name ) {
 		var v = parseInt(el.css(key), 10);
 		if (v) { return v; }
 		var s = el[0].currentStyle; 
-		return s && s.width && parseInt(s.width, 10);	
+		return s && s.width && parseInt(s.width, 10);
 	}
 	
 	function hasEvent(el) {
@@ -154,19 +152,19 @@ jQuery.fn.data = function( name ) {
 	function RangeInput(input, conf) {
 		
 		// private variables
-		var self = this,  
+		var self = this,
 			 css = conf.css, 
-			 root = $("<div><div/><a href='#'/></div>").data("rangeinput", self),	
-			 vertical,		
-			 value,			// current value
-			 origo,			// handle's start point
-			 len,				// length of the range
-			 pos;				// current position of the handle		
+			 root = $("<div><div/><a href='#'/></div>").data("rangeinput", self),
+			 vertical,
+			 value,  // current value
+			 origo,  // handle's start point
+			 len,    // length of the range
+			 pos;    // current position of the handle
 		 
-		// create range	 
-		input.before(root);	
+		// create range
+		input.before(root);
 		
-		var handle = root.addClass(css.slider).find("a").addClass(css.handle), 	
+		var handle = root.addClass(css.slider).find("a").addClass(css.handle),
 			 progress = root.find("div").addClass(css.progress);
 		
 		// get (HTML5) attributes into configuration
@@ -175,29 +173,29 @@ jQuery.fn.data = function( name ) {
 			if (parseFloat(val)) {
 				conf[key] = parseFloat(val, 10);
 			}
-		});			   
+		});
 		
 		var range = conf.max - conf.min, 
 			 step = conf.step == 'any' ? 0 : conf.step,
 			 precision = conf.precision;
-			 
+			
 		if (precision === undefined) {
 			precision = step.toString().split(".");
 			precision = precision.length === 2 ? precision[1].length : 0;
-		}  
+		}
 		
 		// Replace built-in range input (type attribute cannot be changed)
-		if (input.attr("type") == 'range') {			
+		if (input.attr("type") == 'range') {
 			var def = input.clone().wrap("<div/>").parent().html(),
-				 clone = $(def.replace(/type/i, "type=text data-orig-type"));
-				 
+				clone = $(def.replace(/type/i, "type=text data-orig-type"));
+			
 			clone.val(conf.value);
 			input.replaceWith(clone);
 			input = clone;
 		}
 		
 		input.addClass(css.input);
-			 
+		
 		var fire = $(self).add(input), fireOnSlide = true;
 
 		
@@ -209,34 +207,34 @@ jQuery.fn.data = function( name ) {
 			
 			vertical configuration gives additional complexity. 
 		 */
-		function slide(evt, x, val, isSetValue) { 
-
+		function slide(evt, x, val, isSetValue) {
+			
 			// calculate value based on slide position
 			if (val === undefined) {
-				val = x / len * range;  
+				val = x / len * range;
 				
-			// x is calculated based on val. we need to strip off min during calculation	
+			// x is calculated based on val. we need to strip off min during calculation
 			} else if (isSetValue) {
-				val -= conf.min;	
+				val -= conf.min;
 			}
 			
 			// increment in steps
 			if (step) {
 				val = Math.round(val / step) * step;
 			}
-
+			
 			// count x based on value or tweak x if stepping is done
 			if (x === undefined || step) {
-				x = val * len / range;	
-			}  
+				x = val * len / range;
+			}
 			
 			// crazy value?
-			if (isNaN(val)) { return self; }       
+			if (isNaN(val)) { return self; }
 			
 			// stay within range
-			x = Math.max(0, Math.min(x, len));  
-			val = x / len * range;   
-
+			x = Math.max(0, Math.min(x, len));
+			val = x / len * range;
+			
 			if (isSetValue || !vertical) {
 				val += conf.min;
 			}
@@ -246,20 +244,20 @@ jQuery.fn.data = function( name ) {
 				if (isSetValue) {
 					x = len -x;
 				} else {
-					val = conf.max - val;	
+					val = conf.max - val;
 				}
-			}	
+			}
 			
 			// precision
-			val = round(val, precision); 
+			val = round(val, precision);
 			
 			// onSlide
 			var isClick = evt.type == "click";
 			if (fireOnSlide && value !== undefined && !isClick) {
 				evt.type = "onSlide";           
 				fire.trigger(evt, [val, x]); 
-				if (evt.isDefaultPrevented()) { return self; }  
-			}				
+				if (evt.isDefaultPrevented()) { return self; }
+			}
 			
 			// speed & callback
 			var speed = isClick ? conf.speed : 0,
@@ -271,58 +269,58 @@ jQuery.fn.data = function( name ) {
 			if (vertical) {
 				handle.animate({top: x}, speed, callback);
 				if (conf.progress) { 
-					progress.animate({height: len - x + handle.height() / 2}, speed);	
-				}				
-				
+					progress.animate({height: len - x + handle.height() / 2}, speed);
+				}
+			
 			} else {
 				handle.animate({left: x}, speed, callback);
 				if (conf.progress) { 
-					progress.animate({width: x + handle.width() / 2}, speed); 
+					progress.animate({width: x + handle.width() / 2}, speed);
 				}
 			}
 			
 			// store current value
-			value = val; 
-			pos = x;			 
+			value = val;
+			pos = x;
 			
-			// se input field's value
+			// set input field's value
 			input.val(val);
-
+			
 			return self;
-		} 
+		}
 		
 		
-		$.extend(self, {  
+		$.extend(self, {
 			
 			getValue: function() {
-				return value;	
+				return value;
 			},
 			
 			setValue: function(val, e) {
 				init();
-				return slide(e || $.Event("api"), undefined, val, true); 
-			}, 			  
+				return slide(e || $.Event("api"), undefined, val, true);
+			},
 			
 			getConf: function() {
-				return conf;	
+				return conf;
 			},
 			
 			getProgress: function() {
-				return progress;	
+				return progress;
 			},
-
+			
 			getHandle: function() {
-				return handle;	
-			},			
+				return handle;
+			},
 			
 			getInput: function() {
-				return input;	
-			}, 
-				
+				return input;
+			},
+			
 			step: function(am, e) {
 				e = e || $.Event();
 				var step = conf.step == 'any' ? 1 : conf.step;
-				self.setValue(value + step * (am || 1), e);	
+				self.setValue(value + step * (am || 1), e);
 			},
 			
 			// HTML5 compatible name
@@ -341,8 +339,8 @@ jQuery.fn.data = function( name ) {
 		$.each("onSlide,change".split(","), function(i, name) {
 				
 			// from configuration
-			if ($.isFunction(conf[name]))  {
-				$(self).on(name, conf[name]);	
+			if ($.isFunction(conf[name])) {
+				$(self).on(name, conf[name]);
 			}
 			
 			// API methods
@@ -351,19 +349,19 @@ jQuery.fn.data = function( name ) {
 				return self;	
 			};
 		}); 
-			
-
-		// dragging		                                  
-		handle.drag({drag: false}).on("dragStart", function() {
 		
-			/* do some pre- calculations for seek() function. improves performance */			
+		
+		// dragging
+		handle.drag({drag: false}).on("dragStart", function() {
+			
+			/* do some pre- calculations for seek() function. improves performance */
 			init();
 			
 			// avoid redundant event triggering (= heavy stuff)
 			fireOnSlide = hasEvent($(self)) || hasEvent(input);
 			
-				
-		}).on("drag", function(e, y, x) {        
+			
+		}).on("drag", function(e, y, x) { 
 			
 			if (input.is(":disabled")) { return false; } 
 			slide(e, vertical ? y : x); 
@@ -371,55 +369,55 @@ jQuery.fn.data = function( name ) {
 		}).on("dragEnd", function(e) {
 			if (!e.isDefaultPrevented()) {
 				e.type = "change";
-				fire.trigger(e, [value]);	 
+				fire.trigger(e, [value]);
 			}
 			
 		}).click(function(e) {
-			return e.preventDefault();	 
-		});		
+			return e.preventDefault();
+		});
 		
 		// clicking
-		root.click(function(e) { 
-			if (input.is(":disabled") || e.target == handle[0]) { 
-				return e.preventDefault(); 
-			}				  
-			init(); 
+		root.click(function(e) {
+			if (input.is(":disabled") || e.target == handle[0]) {
+				return e.preventDefault();
+			}
+			init();
 			var fix = vertical ? handle.height() / 2 : handle.width() / 2;
-			slide(e, vertical ? len-origo-fix + e.pageY  : e.pageX -origo -fix);  
+			slide(e, vertical ? len-origo-fix + e.pageY  : e.pageX -origo -fix);
 		});
 
 		if (conf.keyboard) {
 			
 			input.keydown(function(e) {
-		
+				
 				if (input.attr("readonly")) { return; }
 				
 				var key = e.keyCode,
 					 up = $([75, 76, 38, 33, 39]).index(key) != -1,
-					 down = $([74, 72, 40, 34, 37]).index(key) != -1;					 
-					 
+					 down = $([74, 72, 40, 34, 37]).index(key) != -1;
+				
 				if ((up || down) && !(e.shiftKey || e.altKey || e.ctrlKey)) {
 				
-					// UP: 	k=75, l=76, up=38, pageup=33, right=39			
+					// UP:   k=75, l=76, up=38, pageup=33, right=39
 					if (up) {
 						self.step(key == 33 ? 10 : 1, e);
-						
-					// DOWN:	j=74, h=72, down=40, pagedown=34, left=37
+					
+					// DOWN: j=74, h=72, down=40, pagedown=34, left=37
 					} else if (down) {
-						self.step(key == 34 ? -10 : -1, e); 
-					} 
+						self.step(key == 34 ? -10 : -1, e);
+					}
 					return e.preventDefault();
-				} 
+				}
 			});
 		}
 		
 		
-		input.blur(function(e) {	
+		input.blur(function(e) {
 			var val = $(this).val();
 			if (val !== value) {
 				self.setValue(val, e);
 			}
-		});    
+		});
 		
 		
 		// HTML5 DOM methods
@@ -462,21 +460,21 @@ jQuery.fn.data = function( name ) {
 	$.fn.rangeinput = function(conf) {
 
 		// already installed
-		if (this.data("rangeinput")) { return this; } 
+		if (this.data("rangeinput")) { return this; }
 		
 		// extend configuration with globals
-		conf = $.extend(true, {}, tool.conf, conf);		
+		conf = $.extend(true, {}, tool.conf, conf);
 		
 		var els;
 		
-		this.each(function() {				
-			var el = new RangeInput($(this), $.extend(true, {}, conf));		 
+		this.each(function() {
+			var el = new RangeInput($(this), $.extend(true, {}, conf));
 			var input = el.getInput().data("rangeinput", el);
-			els = els ? els.add(input) : input;	
-		});		
+			els = els ? els.add(input) : input;
+		});
 		
 		return els ? els : this; 
-	};	
+	};
 	
 	
 }) (jQuery);
