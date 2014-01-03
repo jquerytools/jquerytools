cf7db74cd916be95c32f18879ab0d7c884534a07
Dj Gilcrease <dj.gilcrease@kanbansolutions.com>
2014-01-03 14:24:11 -0500

    Add Bower and Gruntjs support

--- src/scrollable/scrollable.autoscroll.js
+++ src/scrollable/scrollable.autoscroll.js
@@ -1,97 +1,97 @@
 /**
- * @license 
+ * @license
  * jQuery Tools @VERSION / Scrollable Autoscroll
- * 
+ *
  * NO COPYRIGHTS OR LICENSES. DO WHAT YOU LIKE.
- * 
+ *
  * http://flowplayer.org/tools/scrollable/autoscroll.html
  *
  * Since: September 2009
- * Date: @DATE 
+ * Date: @DATE
  */
-(function($) {		
+(function($) {
 
-	var t = $.tools.scrollable; 
-	
-	t.autoscroll = {
-		
-		conf: {
-			autoplay: true,
-			interval: 3000,
-			autopause: true
-		}
-	};	
-	
-	// jQuery plugin implementation
-	$.fn.autoscroll = function(conf) { 
+    var t = $.tools.scrollable;
 
-		if (typeof conf == 'number') {
-			conf = {interval: conf};	
-		}
-		
-		var opts = $.extend({}, t.autoscroll.conf, conf), ret;
-		
-		this.each(function() {		
-				
-			var api = $(this).data("scrollable"),
-			    root = api.getRoot(),
-			    // interval stuff
-    			timer, stopped = false;
+    t.autoscroll = {
 
-	    /**
+        conf: {
+            autoplay: true,
+            interval: 3000,
+            autopause: true
+        }
+    };
+
+    // jQuery plugin implementation
+    $.fn.autoscroll = function(conf) {
+
+        if (typeof conf == 'number') {
+            conf = {interval: conf};
+        }
+
+        var opts = $.extend({}, t.autoscroll.conf, conf), ret;
+
+        this.each(function() {
+
+            var api = $(this).data("scrollable"),
+                root = api.getRoot(),
+                // interval stuff
+                timer, stopped = false;
+
+        /**
       *
       *   Function to run autoscroll through event binding rather than setInterval
       *   Fixes this bug: http://flowplayer.org/tools/forum/25/72029
       */
-      function scroll(){        
-      	// Fixes https://github.com/jquerytools/jquerytools/issues/591
+      function scroll(){
+          // Fixes https://github.com/jquerytools/jquerytools/issues/591
         if (timer) clearTimeout(timer); // reset timeout, especially for onSeek event
         timer = setTimeout(function(){
           api.next();
         }, opts.interval);
       }
-			    
-			if (api) { ret = api; }
-			
-			api.play = function() { 
-				
-				// do not start additional timer if already exists
-				if (timer) { return; }
-				
-				stopped = false;
-				root.on('onSeek', scroll);
-				scroll();
-			};	
 
-			api.pause = function() {
-				timer = clearTimeout(timer);  // clear any queued items immediately
-				root.off('onSeek', scroll);
-			};
-			
-			// resume playing if not stopped
-			api.resume = function() {
-				stopped || api.play();
-			};
-			
-			// when stopped - mouseover won't restart 
-			api.stop = function() {
-			  stopped = true;
-				api.pause();
-			};
-		
-			/* when mouse enters, autoscroll stops */
-			if (opts.autopause) {
-				root.add(api.getNaviButtons()).hover(api.pause, api.resume);
-			}
-			
-			if (opts.autoplay) {
-				api.play();				
-			}
+            if (api) { ret = api; }
 
-		});
-		
-		return opts.api ? ret : this;
-		
-	}; 
-	
-})(jQuery);		
+            api.play = function() {
+
+                // do not start additional timer if already exists
+                if (timer) { return; }
+
+                stopped = false;
+                root.on('onSeek', scroll);
+                scroll();
+            };
+
+            api.pause = function() {
+                timer = clearTimeout(timer);  // clear any queued items immediately
+                root.off('onSeek', scroll);
+            };
+
+            // resume playing if not stopped
+            api.resume = function() {
+                stopped || api.play();
+            };
+
+            // when stopped - mouseover won't restart
+            api.stop = function() {
+              stopped = true;
+                api.pause();
+            };
+
+            /* when mouse enters, autoscroll stops */
+            if (opts.autopause) {
+                root.add(api.getNaviButtons()).hover(api.pause, api.resume);
+            }
+
+            if (opts.autoplay) {
+                api.play();
+            }
+
+        });
+
+        return opts.api ? ret : this;
+
+    };
+
+})(jQuery);
--- src/tabs/tabs.slideshow.js
+++ src/tabs/tabs.slideshow.js
@@ -1,53 +1,53 @@
 /**
- * @license 
+ * @license
  * jQuery Tools @VERSION Slideshow - Extend it.
- * 
+ *
  * NO COPYRIGHTS OR LICENSES. DO WHAT YOU LIKE.
- * 
+ *
  * http://flowplayer.org/tools/tabs/slideshow.html
  *
  * Since: September 2009
- * Date: @DATE 
+ * Date: @DATE
  */
 (function($) {
-	
-	var tool;
-	
-	tool = $.tools.tabs.slideshow = { 
 
-		conf: {
-			next: '.forward',
-			prev: '.backward',
-			disabledClass: 'disabled',
-			autoplay: false,
-			autopause: true,
-			interval: 3000, 
-			clickable: true,
-			api: false
-		}
-	};  
-	
-	function Slideshow(root, conf) {
-	
-		var self = this,
-			 fire = root.add(this),
-			 tabs = root.data("tabs"),
-			 timer, 
-			 stopped = true;
-		
-		// next / prev buttons
-		function find(query) {
-			var el = $(query);
-			return el.length < 2 ? el : root.parent().find(query);	
-		}	
-		
-		var nextButton = find(conf.next).click(function() {
-			tabs.next();		
-		});
-		
-		var prevButton = find(conf.prev).click(function() {
-			tabs.prev();		
-		}); 
+    var tool;
+
+    tool = $.tools.tabs.slideshow = {
+
+        conf: {
+            next: '.forward',
+            prev: '.backward',
+            disabledClass: 'disabled',
+            autoplay: false,
+            autopause: true,
+            interval: 3000,
+            clickable: true,
+            api: false
+        }
+    };
+
+    function Slideshow(root, conf) {
+
+        var self = this,
+             fire = root.add(this),
+             tabs = root.data("tabs"),
+             timer,
+             stopped = true;
+
+        // next / prev buttons
+        function find(query) {
+            var el = $(query);
+            return el.length < 2 ? el : root.parent().find(query);
+        }
+
+        var nextButton = find(conf.next).click(function() {
+            tabs.next();
+        });
+
+        var prevButton = find(conf.prev).click(function() {
+            tabs.prev();
+        });
 
     /**
     *
@@ -55,139 +55,141 @@
     */
     function next(){
       // Fixes https://github.com/jquerytools/jquerytools/issues/885
-      if (timer) clearTimeout(timer); // reset timeout, especially for angry clicks
+      if (timer) {
+        clearTimeout(timer); // reset timeout, especially for angry clicks
+      }
       timer = setTimeout(function(){
         tabs.next();
       }, conf.interval);
     }
 
-		// extend the Tabs API with slideshow methods			
-		$.extend(self, {
-				
-			// return tabs API
-			getTabs: function() {
-				return tabs;	
-			},
-			
-			getConf: function() {
-				return conf;	
-			},
-				
-			play: function() {
-	
-				// do not start additional timer if already exists
-				if (timer) { return self; }	
-				
-				// onBeforePlay
-				var e = $.Event("onBeforePlay");
-				fire.trigger(e);				
-				if (e.isDefaultPrevented()) { return self; }				
-				
-				stopped = false;				
-				
-				// onPlay
-				fire.trigger("onPlay");				
-				
-				fire.on('onClick', next);
-				next();
-				
-				return self;
-			},
-		
-			pause: function() {
-				
-				if (!timer) { return self; }
+        // extend the Tabs API with slideshow methods
+        $.extend(self, {
 
-				// onBeforePause
-				var e = $.Event("onBeforePause");
-				fire.trigger(e);					
-				if (e.isDefaultPrevented()) { return self; }		
-				
-				timer = clearTimeout(timer);
-				
-				// onPause
-				fire.trigger("onPause");	
-				
-				fire.off('onClick', next);
-				
-				return self;
-			},
-			
-			// resume playing if not stopped
-			resume: function() {
-				stopped || self.play();
-			},
-			
-			// when stopped - mouseover won't restart 
-			stop: function() {					
-				self.pause();
-				stopped = true;	
-			}
-			
-		});
+            // return tabs API
+            getTabs: function() {
+                return tabs;
+            },
 
-		// callbacks	
-		$.each("onBeforePlay,onPlay,onBeforePause,onPause".split(","), function(i, name) {
-				
-			// configuration
-			if ($.isFunction(conf[name]))  {
-				$(self).on(name, conf[name]);	
-			}
-			
-			// API methods				
-			self[name] = function(fn) {
-				return $(self).on(name, fn);
-			};
-		});	
-		
-	
-		/* when mouse enters, slideshow stops */
-		if (conf.autopause) {
-			tabs.getTabs().add(nextButton).add(prevButton).add(tabs.getPanes()).hover(self.pause, self.resume);
-		} 
-		
-		if (conf.autoplay) {
-			self.play();	
-		}
-		
-		if (conf.clickable) {
-			tabs.getPanes().click(function()  {
-				tabs.next();
-			});
-		} 
-		
-		// manage disabling of next/prev buttons
-		if (!tabs.getConf().rotate) {
-			
-			var disabled = conf.disabledClass;
-			
-			if (!tabs.getIndex()) {
-				prevButton.addClass(disabled);
-			}
-			
-			tabs.onBeforeClick(function(e, i)  { 
-				prevButton.toggleClass(disabled, !i);
-				nextButton.toggleClass(disabled, i == tabs.getTabs().length -1); 
-			});
-		}  
-	}
-	
-	// jQuery plugin implementation
-	$.fn.slideshow = function(conf) {
-	
-		// return existing instance
-		var el = this.data("slideshow");
-		if (el) { return el; }
- 
-		conf = $.extend({}, tool.conf, conf);
-		
-		this.each(function() {
-			el = new Slideshow($(this), conf);
-			$(this).data("slideshow", el); 			
-		});	
-		
-		return conf.api ? el : this;
-	};
-	
-})(jQuery); 
+            getConf: function() {
+                return conf;
+            },
+
+            play: function() {
+
+                // do not start additional timer if already exists
+                if (timer) { return self; }
+
+                // onBeforePlay
+                var e = $.Event("onBeforePlay");
+                fire.trigger(e);
+                if (e.isDefaultPrevented()) { return self; }
+
+                stopped = false;
+
+                // onPlay
+                fire.trigger("onPlay");
+
+                fire.on('onClick', next);
+                next();
+
+                return self;
+            },
+
+            pause: function() {
+
+                if (!timer) { return self; }
+
+                // onBeforePause
+                var e = $.Event("onBeforePause");
+                fire.trigger(e);
+                if (e.isDefaultPrevented()) { return self; }
+
+                timer = clearTimeout(timer);
+
+                // onPause
+                fire.trigger("onPause");
+
+                fire.off('onClick', next);
+
+                return self;
+            },
+
+            // resume playing if not stopped
+            resume: function() {
+                stopped || self.play();
+            },
+
+            // when stopped - mouseover won't restart
+            stop: function() {
+                self.pause();
+                stopped = true;
+            }
+
+        });
+
+        // callbacks
+        $.each("onBeforePlay,onPlay,onBeforePause,onPause".split(","), function(i, name) {
+
+            // configuration
+            if ($.isFunction(conf[name]))  {
+                $(self).on(name, conf[name]);
+            }
+
+            // API methods
+            self[name] = function(fn) {
+                return $(self).on(name, fn);
+            };
+        });
+
+
+        /* when mouse enters, slideshow stops */
+        if (conf.autopause) {
+            tabs.getTabs().add(nextButton).add(prevButton).add(tabs.getPanes()).hover(self.pause, self.resume);
+        }
+
+        if (conf.autoplay) {
+            self.play();
+        }
+
+        if (conf.clickable) {
+            tabs.getPanes().click(function()  {
+                tabs.next();
+            });
+        }
+
+        // manage disabling of next/prev buttons
+        if (!tabs.getConf().rotate) {
+
+            var disabled = conf.disabledClass;
+
+            if (!tabs.getIndex()) {
+                prevButton.addClass(disabled);
+            }
+
+            tabs.onBeforeClick(function(e, i)  {
+                prevButton.toggleClass(disabled, !i);
+                nextButton.toggleClass(disabled, i == tabs.getTabs().length -1);
+            });
+        }
+    }
+
+    // jQuery plugin implementation
+    $.fn.slideshow = function(conf) {
+
+        // return existing instance
+        var el = this.data("slideshow");
+        if (el) { return el; }
+
+        conf = $.extend({}, tool.conf, conf);
+
+        this.each(function() {
+            el = new Slideshow($(this), conf);
+            $(this).data("slideshow", el);
+        });
+
+        return conf.api ? el : this;
+    };
+
+})(jQuery);
 
